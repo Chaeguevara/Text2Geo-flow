1b090f9ff68c0e28ae6c3bc6dd8c70c9b84812ff
Chaeguevara <dfso2@naver.com>
2022-09-19 22:26:21 +0900

    Duplicated geo lv 2 (Si-gun-gu) to Json

--- Utils/kor_place2dict.py
+++ Utils/kor_place2dict.py
@@ -1,3 +1,5 @@
+from __future__ import nested_scopes
+from numpy.core.fromnumeric import sort
 import pandas as pd
 import json
 import numpy as np
@@ -20,6 +22,15 @@ def handle_dup_siGunGu(geo_lv2):
     sigungu_name_count = sigungu["시군구"].value_counts()  # 모든 시군구이름 중복갯수
     dup_sigungu_name = sigungu_name_count.\
         loc[sigungu_name_count != 1].index  # 중복 시군구 ['중구','동구',...]
+    dup_sigungu_json = sigungu_name_count.to_json()
+    with open("../Data/lv2_dup_name.json", "w") as f:
+        f.write(
+            json.dumps(
+                json.loads(dup_sigungu_json),
+                indent=2,
+                sort_keys=False,
+                ensure_ascii=False)
+        )
 
     def dup_engname_to_nest(
             x, dup_sigungu_name, sido, sigungu, sido_in_eng_dict
@@ -50,16 +61,17 @@ def handle_dup_siGunGu(geo_lv2):
             sido_in_eng_dict
         )
     )
+    geo_lv2["지명 갯수"] = 1
+    geo_lv2[geo_lv2["소분류"].isna()] = sigungu
     return sigungu
 
 
 def write_geo_lv2(kor_geo_data):
     geo_lv2 = kor_geo_data.copy()
     nested_sigungu_df = handle_dup_siGunGu(geo_lv2)
-    nested_sigungu_df = nested_sigungu_df.copy()
-    geo_lv2 = nested_sigungu_df[nested_sigungu_df.T.count() == 5]
+    geo_lv2 = nested_sigungu_df.copy()
+    geo_lv2 = geo_lv2[geo_lv2.T.count() == 5]
     geo_lv2.drop(columns=["읍면동", "소분류", "중분류"], inplace=True)
-    print(geo_lv2)
     j_lv2 = geo_lv2.groupby(["시도"])\
         .apply(lambda x: x[["시군구", "영문 표기", "지명 갯수"]].to_dict('records'))\
         .reset_index()\
@@ -72,7 +84,7 @@ def write_geo_lv2(kor_geo_data):
                        sort_keys=False,
                        ensure_ascii=False)
         )
-    return
+    return nested_sigungu_df
 
 
 def write_geo_lv1(kor_geo_data):
@@ -81,7 +93,7 @@ def write_geo_lv1(kor_geo_data):
     geo_lv1.drop(columns=["읍면동", "시군구"], inplace=True)
     geo_lv1_json = geo_lv1.to_json(orient="records")
 
-    with open("lv1_korea_geo.json", "w") as outfile:
+    with open("../Data/lv1_korea_geo.json", "w") as outfile:
         outfile.write(
             json.dumps(
                 json.loads(geo_lv1_json),
@@ -92,7 +104,14 @@ def write_geo_lv1(kor_geo_data):
             )
 
 
+def write_geo_lv3(kor_geo_data):
+    u_myeon_dong = kor_geo_data[kor_geo_data["소분류"].notna()]
+    print(kor_geo_data["소분류"].notna())
+    return
+
+
 if __name__ == "__main__":
     kor_geo_data = get_data_from_csv()
-    write_geo_lv2(kor_geo_data)
+    nested_sigungu_df = write_geo_lv2(kor_geo_data)
+    write_geo_lv3(nested_sigungu_df)
 
